cb733dd0d4316b5765cb683374939581d6761744
Ellen Bennett <bennette831@gmail.com>
2023-06-13 21:43:24 -0600

    working deliverables, using state variables to dynamically update the DOM

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+  let [cartItem, setCart] = useState(false)
+
+  function addCart () {
+    setCart(() => cartItem = !cartItem)
+  }
+
+  const liClass = cartItem ? "in-cart" : ""
+
   return (
-    <li className="">
+    <li className={liClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={addCart}>{cartItem ? "Remove From Cart" : "Add to Cart"}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+
+  const [selectedCategory, setCategory] = useState("All")
+  const [disItems, setItems] = useState(items)
+
+  function handleChange (event) {
+    setCategory(category => category = event.target.value)
+
+    const newItemArr = items.filter((item) => {
+      return item.category == event.target.value
+    })
+
+    setItems(newItemArr)
+  }
+
+  const displayItems = disItems.map((item) => (
+        <Item key={item.id} name={item.name} category={item.category} />
+  ))
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleChange} >
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,9 +31,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
+        {displayItems}
       </ul>
     </div>
   );
